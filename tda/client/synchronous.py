57e112e1a2b44d4a3e73ede183a32023034eb3b6
Rekesh Ali <rekeshali@gmail.com>
2021-11-01 00:01:13 -0500

    typed request functions not working, had to use generic handle and pass requests.. it worked in some env and not in others which is weird but here's a fix so im done

--- tda/client/synchronous.py
+++ tda/client/synchronous.py
@@ -14,7 +14,8 @@ class Client(BaseClient):
         self.logger.debug('Req {}: GET to {}, params={}'.format(
             req_num, dest, json.dumps(params, indent=4)))
 
-        resp = self.session.get(dest, params=params)
+        # resp = self.session.get(dest, params=params)
+        resp = self.session.request('get', dest, params=params)
         self._log_response(resp, req_num)
         register_redactions_from_response(resp)
         return resp
@@ -28,7 +29,8 @@ class Client(BaseClient):
         self.logger.debug('Req {}: POST to {}, json={}'.format(
             req_num, dest, json.dumps(data, indent=4)))
 
-        resp = self.session.post(dest, json=data)
+        # resp = self.session.post(dest, json=data)
+        resp = self.session.request('post', dest, json=data)
         self._log_response(resp, req_num)
         register_redactions_from_response(resp)
         return resp
@@ -42,7 +44,8 @@ class Client(BaseClient):
         self.logger.debug('Req {}: PUT to {}, json={}'.format(
             req_num, dest, json.dumps(data, indent=4)))
 
-        resp = self.session.put(dest, json=data)
+        # resp = self.session.put(dest, json=data)
+        resp = self.session.request('put', dest, json=data)
         self._log_response(resp, req_num)
         register_redactions_from_response(resp)
         return resp
@@ -56,7 +59,8 @@ class Client(BaseClient):
         self.logger.debug('Req {}: PATCH to {}, json={}'.format(
             req_num, dest, json.dumps(data, indent=4)))
 
-        resp = self.session.patch(dest, json=data)
+        # resp = self.session.patch(dest, json=data)
+        resp = self.session.request('patch', dest, json=data)
         self._log_response(resp, req_num)
         register_redactions_from_response(resp)
         return resp
@@ -69,7 +73,8 @@ class Client(BaseClient):
         req_num = self._req_num()
         self.logger.debug('Req {}: DELETE to {}'.format(req_num, dest))
 
-        resp = self.session.delete(dest)
+        # resp = self.session.delete(dest)
+        resp = self.session.request('delete', dest)
         self._log_response(resp, req_num)
         register_redactions_from_response(resp)
         return resp
